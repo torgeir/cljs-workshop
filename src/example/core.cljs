9a9863572e22ebaeabf88b505573ae5cf0ea4f37
Kjetil Midtgarden Golid <kjetil.golid@gmail.com>
2019-03-25 09:22:40 +0100

    Move sketch into own file, for future scalability

--- src/example/core.cljs
+++ src/example/core.cljs
@@ -1,45 +1,14 @@
 (ns example.core
   (:require [clojure.pprint :as pp]
             [goog.dom :as dom]
-            [quil.core :as q]
-            [quil.middleware :as m]))
+            [example.perlin_flow :as pf]
+            [example.tileset :as ts]))
+
 
 (enable-console-print!)
 
+
 (defonce sketch (dom/getElement "sketch"))
 
-(def w 1200)
-(def h 800)
-(def ndim 150)
-(def size 500)
 
-(defn particle [] {:x (* w (Math/random)) :y (* h (Math/random)) :vx 0 :vy 0 :adir 0})
-(defn particles [n] (map (fn [] (particle)) (range n)))
-
-(defn update-pos [curr delta max] (mod (+ curr delta) max))
-(defn update-vel [curr delta] (q/norm (+ curr delta) 0 2))
-(defn update-acc [curr x y] (q/map-range (q/noise (/ x ndim) (/ y ndim)) 0 1 (- Math/PI) Math/PI))
-
-(defn draw [s]
-  (q/fill 255 0 0 25)
-  (doseq [pnt s] (q/ellipse (:x pnt) (:y pnt) 1 1)))
-
-(defn update-particle [p]
-  (assoc p
-         :x  (update-pos (:x p) (:vx p) w)
-         :y  (update-pos (:y p) (:vy p) h)
-         :vx (update-vel (:vx p) (Math/cos (:adir p)))
-         :vy (update-vel (:vx p) (Math/sin (:adir p)))
-         :adir (update-acc (:adir p) (:x p) (:y p))))
-
-(defn create []
-  (q/defsketch perlin-flow
-    :host sketch
-    :size [w h]
-    :setup (fn []
-             (q/no-stroke)
-             (q/background 20)
-             (particles size))
-    :update (fn [s] (map update-particle s))
-    :draw draw
-    :middleware [m/fun-mode]))
+(defonce s (ts/create-tileset sketch))
---
+++ src/example/perlin_flow.cljs
@@ -0,0 +1,39 @@
+(ns example.perlin_flow
+  (:require [quil.core :as q]
+            [quil.middleware :as m]))
+
+(def w 1200)
+(def h 800)
+(def ndim 350)
+(def size 500)
+
+(defn particle [] {:x (* w (Math/random)) :y (* h (Math/random)) :vx 0 :vy 0 :adir 0})
+(defn particles [n] (map (fn [] (particle)) (range n)))
+
+(defn update-pos [curr delta max] (mod (+ curr delta) max))
+(defn update-vel [curr delta] (q/norm (+ curr delta) 0 2))
+(defn update-acc [curr x y] (q/map-range (q/noise (/ x ndim) (/ y ndim)) 0 1 (- Math/PI) Math/PI))
+
+(defn draw [s]
+  (q/fill 255 0 100 25)
+  (doseq [pnt s] (q/ellipse (:x pnt) (:y pnt) 1 1)))
+
+(defn update-particle [p]
+  (assoc p
+         :x  (update-pos (:x p) (:vx p) w)
+         :y  (update-pos (:y p) (:vy p) h)
+         :vx (update-vel (:vx p) (Math/cos (:adir p)))
+         :vy (update-vel (:vx p) (Math/sin (:adir p)))
+         :adir (update-acc (:adir p) (:x p) (:y p))))
+
+(defn create-perlin-flow [canvas]
+  (q/defsketch perlin-flow
+    :host canvas
+    :size [w h]
+    :setup (fn []
+             (q/no-stroke)
+             (q/background 20)
+             (particles size))
+    :update (fn [s] (map update-particle s))
+    :draw draw
+    :middleware [m/fun-mode]))
