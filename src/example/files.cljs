556595693a8941c84fcf5ecc3d365349981784bb
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2019-06-12 19:46:40 +0200

    make download-jpeg work in chrome as well using URL

--- src/example/files.cljs
+++ src/example/files.cljs
@@ -1,11 +1,20 @@
 (ns example.files)
 
 
-(defn download-jpeg [canvas filename]
-  (let [url  (.toDataURL canvas "image/jpeg")
-        link (.createElement js/document "a")]
-    (.appendChild (.-body js/document) link)
-    (set! (.-href link) url)
-    (set! (.-download link) (str filename ".jpeg"))
-    (.click link)
-    (.removeChild (.-body js/document) link)))
+(defn download-jpeg
+  "Convert canvas to blob, create a link to its object url and click it to trigger a jpeg file download.
+  Like described in https://stackoverflow.com/questions/19327749/javascript-blob-filename-without-link."
+  [canvas filename]
+  (.toBlob canvas
+           (fn [blob]
+             (let [link (.createElement js/document "a")
+                   url  (.createObjectURL js/URL blob)]
+               (.appendChild (.-body js/document) link)
+               (set! (.-href link) url)
+               (set! (.-download link) (str filename ".jpeg"))
+               (.click link)
+               (js/setTimeout
+                 #(do
+                    (.removeChild (.-body js/document) link)
+                    (.revokeObjectURL js/URL url)))))
+           "image/jpeg"))
