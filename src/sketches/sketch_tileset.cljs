1f78153d3d28f6052c22397be39f4cffa8548c6c
Kjetil Midtgarden Golid <kjetil.golid@gmail.com>
2019-06-17 18:33:55 +0200

    Rename draw-square and make it more flexible with some preset shape functions

--- src/sketches/sketch_tileset.cljs
+++ src/sketches/sketch_tileset.cljs
@@ -15,6 +15,21 @@
 (def palette (find-palette "cc242"))
 
 
+(defn draw-square [x1 y1 x2 y2 c1]
+  (apply q/fill c1)
+  (q/rect x1 y1 x2 y2))
+
+(defn draw-diagonal-square [x1 y1 x2 y2 c1 c2 dir]
+  (draw-square x1 y1 x2 y2 c1)
+  (apply q/fill c2)
+  (q/begin-shape :triangles)
+  (q/vertex x1 y1)
+  (if (= "inc" dir)
+    (q/vertex x2 y1)
+    (q/vertex x2 y2))
+  (q/vertex x1 y2)
+  (q/end-shape))
+
 (defn generate-square [depth]
   (if (= depth 0)
     (if (< (rand) .7) \w \b)
@@ -29,29 +44,27 @@
         :else          [ul \- ur \/ dl \- dr]))))
 
 
-(defn draw-square [cells size]
-  (doseq [cell cells]
-    (case cell
-      \w ()
-      \b (do
-           (apply q/fill (rand-nth (:colors palette)))
-           (q/rect spacing spacing size size))
-      \- (q/translate size 0)
-      \/ (q/translate (- size) size)
-      (draw-square cell (/ size 2))))
-  (q/translate (- size) (- size)))
-
-
-(defn sketch-update [state]
-  (generate-square depth))
-
+(defn draw-segment [cells size]
+  ;;(let [sx size sy (q/ceil (/ size 3))]
+  (let [sx size sy size]
+    (doseq [cell cells]
+      (let [cols (shuffle (:colors palette))]
+        (case cell
+          \w (draw-diagonal-square spacing spacing sx sy (nth cols 0) (nth cols 1) "inc")
+          \b (draw-diagonal-square spacing spacing sx sy (nth cols 0) (nth cols 1) "dec")
+          ;; \w (draw-square spacing spacing sx sy (nth cols 0))
+          ;; \b (draw-square spacing spacing sx sy (nth cols 0))
+          \- (q/translate sx 0)
+          \/ (q/translate (- sx) sy)
+          (draw-segment cell (/ size 2)))))
+    (q/translate (- sx) (- sy))))
 
 (defn sketch-draw [state]
   (apply q/background (:background palette))
   (q/translate padding-horizontal (/ (- h
                                         (- w (* 2 padding-horizontal)))
                                      2))
-  (draw-square state (- (/ w 2) padding-horizontal)))
+  (draw-segment state (- (/ w 2) padding-horizontal)))
 
 
 (defn create [canvas]
