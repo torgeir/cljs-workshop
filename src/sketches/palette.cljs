7251e809d7027b15267e2e2baba9dcd2d33654eb
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2019-09-17 15:56:09 +0200

    Update palettes with the latest colors from kgolid/chromotome
    https://github.com/kgolid/chromotome/

--- src/sketches/palette.cljs
+++ src/sketches/palette.cljs
@@ -10,8 +10,8 @@
   "Color palettes from https://kgolid.github.io/chromotome-site/"
   (map (fn [palette]
          (-> palette
-             (update :colors #(map hex-to-rgb %))
-             (update :background hex-to-rgb)))
+           (update :colors #(map hex-to-rgb %))
+           (update :background #(hex-to-rgb (or % #"#f2e8e4")))))
        [{:name "frozen-rose" :colors ["#29368f" "#e9697b" "#1b164d" "#f7d996"] :background "#f2e8e4"}
         {:name "winter-night" :colors ["#122438" "#dd672e" "#87c7ca" "#ebebeb"] :background "#ebebeb"}
         {:name "saami" :colors ["#eab700" "#e64818" "#2c6393" "#eecfca"] :background "#e7e6e4"}
@@ -20,7 +20,19 @@
         {:name "tricolor" :colors ["#ec643b" "#56b7ab" "#f8cb57" "#1f1e43"] :background "#f7f2df"}
         {:name "foxshelter" :colors ["#ff3931" "#007861" "#311f27" "#bab9a4"] :background "#dddddd"}
         {:name "hermes" :colors ["#253852" "#51222f" "#b53435" "#ecbb51"] :background "#eeccc2"}
-
+        {:name "olympia" :colors ["#ff3250" "#ffb33a" "#008c36" "#0085c6" "#4c4c4c"] :stroke "#0b0b0b" :background "#faf2e5"}
+        {:name "byrnes" :colors ["#c54514" "#dca215" "#23507f"] :stroke "#0b0b0b" :background "#e8e7d4"}
+        {:name "butterfly" :colors ["#f40104" "#f6c0b3" "#99673a" "#f0f1f4"] :stroke "#191e36" :background "#191e36"}
+        {:name "floratopia" :colors ["#bf4a2b" "#cd902a" "#4e4973" "#f5d4bc"] :stroke "#1e1a43" :background "#1e1a43"}
+        {:name "verena" :colors ["#f1594a" "#f5b50e" "#14a160" "#2969de" "#885fa4"] :stroke "#1a1a1a" :background "#e2e6e8"}
+        {:name "empusa" :colors ["#c92a28" "#e69301" "#1f8793" "#13652b" "#e7d8b0" "#48233b" "#e3b3ac"] :stroke "#1a1a1a" :background "#f0f0f2"}
+        {:name "florida_citrus" :colors ["#ea7251" "#ebf7f0" "#02aca5"] :stroke "#050100" :background "#9ae2d3"}
+        {:name "lemon_citrus" :colors ["#e2d574" "#f1f4f7" "#69c5ab"] :stroke "#463231" :background "#f79eac"}
+        {:name "yuma_punk" :colors ["#f05e3b" "#ebdec4" "#ffdb00"] :stroke "#ebdec4" :background "#161616"}
+        {:name "moir" :colors ["#a49f4f" "#d4501e" "#f7c558" "#ebbaa6"] :stroke "#161716" :background "#f7f4ef"}
+        {:name "tokyo" :colors ["#d13821" "#1d295b" "#51587d" "#e7e7e7"] :stroke "#0b0e3e" :background "#c7b09e"}
+        {:name "bauhaus01" :colors ["#ea542f" "#f19c1b" "#4f8ba9"] :stroke "#221e1f" :background "#e7dbc4"}
+        {:name "bauhaus02" :colors ["#bb2f2a" "#e9b500" "#0165b7"] :stroke "#000000" :background "#e5d6b8"}
         {:name "cc239" :colors ["#e3dd34" "#78496b" "#f0527f" "#a7e0e2"] :background "#e0eff0"}
         {:name "cc234" :colors ["#ffce49" "#ede8dc" "#ff5736" "#ff99b4"] :background "#f7f4ed"}
         {:name "cc232" :colors ["#5c5f46" "#ff7044" "#ffce39" "#66aeaa"] :background "#e9ecde"}
@@ -28,8 +40,27 @@
         {:name "cc242" :colors ["#bbd444" "#fcd744" "#fa7b53" "#423c6f"] :background "#faf4e4"}
         {:name "cc245" :colors ["#0d4a4e" "#ff947b" "#ead3a2" "#5284ab"] :background "#f6f4ed"}
         {:name "cc273" :colors ["#363d4a" "#7b8a56" "#ff9369" "#f4c172"] :background "#f0efe2"}
-
-
+        {:name "rag-mysore" :colors ["#ec6c26" "#613a53" "#e8ac52" "#639aa0"] :background "#d5cda1"}
+        {:name "rag-gol" :colors ["#d3693e" "#803528" "#f1b156" "#90a798"] :background "#f0e0a4"}
+        {:name "rag-belur" :colors ["#f46e26" "#68485f" "#3d273a" "#535d55"] :background "#dcd4a6"}
+        {:name "rag-bangalore" :colors ["#ea720e" "#ca5130" "#e9c25a" "#52534f"] :background "#f9ecd3"}
+        {:name "rag-taj" :colors ["#ce565e" "#8e1752" "#f8a100" "#3ac1a6"] :background "#efdea2"}
+        {:name "rag-virupaksha" :colors ["#f5736a" "#925951" "#feba4c" "#9d9b9d"] :background "#eedfa2"}
+        {:name "retro" :colors ["#69766f" "#9ed6cb" "#f7e5cc" "#9d8f7f" "#936454" "#bf5c32" "#efad57"]}
+        {:name "retro-washedout" :colors ["#878a87" "#cbdbc8" "#e8e0d4" "#b29e91" "#9f736c" "#b76254" "#dfa372"]}
+        {:name "roygbiv-warm" :colors ["#705f84" "#687d99" "#6c843e" "#fc9a1a" "#dc383a" "#aa3a33" "#9c4257"]}
+        {:name "roygbiv-toned" :colors ["#817c77" "#396c68" "#89e3b7" "#f59647" "#d63644" "#893f49" "#4d3240"]}
+        {:name "present-correct" :colors ["#fd3741" "#fe4f11" "#ff6800" "#ffa61a" "#ffc219" "#ffd114" "#fcd82e" "#f4d730" "#ced562" "#8ac38f" "#79b7a0" "#72b5b1" "#5b9bae" "#6ba1b7" "#49619d" "#604791" "#721e7f" "#9b2b77" "#ab2562" "#ca2847"]}
+        {:name "tundra1" :colors ["#40708c" "#8e998c" "#5d3f37" "#ed6954" "#f2e9e2"]}
+        {:name "tundra2" :colors ["#5f9e93" "#3d3638" "#733632" "#b66239" "#b0a1a4" "#e3dad2"]}
+        {:name "tundra3" :colors ["#87c3ca" "#7b7377" "#b2475d" "#7d3e3e" "#eb7f64" "#d9c67a" "#f3f2f2"]}
+        {:name "tundra4" :colors ["#d53939" "#b6754d" "#a88d5f" "#524643" "#3c5a53" "#7d8c7c" "#dad6cd"]}
+        {:name "rohlfs_1R" :colors ["#004996" "#567bae" "#ff4c48" "#ffbcb3"] :stroke "#004996" :background "#fff8e7"}
+        {:name "rohlfs_1Y" :colors ["#004996" "#567bae" "#ffc000" "#ffdca4"] :stroke "#004996" :background "#fff8e7"}
+        {:name "rohlfs_1G" :colors ["#004996" "#567bae" "#60bf3c" "#d2deb1"] :stroke "#004996" :background "#fff8e7"}
+        {:name "rohlfs_2" :colors ["#4d3d9a" "#f76975" "#ffffff" "#eff0dd"] :stroke "#211029" :background "#58bdbc"}
+        {:name "rohlfs_3" :colors ["#abdfdf" "#fde500" "#58bdbc" "#eff0dd"] :stroke "#211029" :background "#f76975"}
+        {:name "rohlfs_4" :colors ["#fde500" "#2f2043" "#f76975" "#eff0dd"] :stroke "#211029" :background "#fbbeca"}
         {:name "ducci_jb" :colors ["#395e54" "#e77b4d" "#050006" "#e55486"] :stroke "#050006" :background "#efe0bc"}
         {:name "ducci_a" :colors ["#809498" "#d3990e" "#000000" "#ecddc5"] :stroke "#ecddc5" :background "#863f52"}
         {:name "ducci_b" :colors ["#ecddc5" "#79b27b" "#000000" "#ac6548"] :stroke "#ac6548" :background "#d5c08e"}
@@ -45,43 +76,43 @@
         {:name "ducci_u" :colors ["#f6d700" "#f2d692" "#000000" "#5d3552"] :stroke "#5d3552" :background "#ff7426"}
         {:name "ducci_v" :colors ["#c65f75" "#d3990e" "#000000" "#597e7a"] :stroke "#597e7a" :background "#f6eccb"}
         {:name "ducci_x" :colors ["#dd614a" "#f5cedb" "#1a1e4f"] :stroke "#1a1e4f" :background "#fbb900"}
-
-
-        {:name "iiso_zeitung" :colors ["#ee8067" "#f3df76" "#00a9c0" "#f7ab76"] :stroke "#111a17" :background "#f5efcb"}
-        {:name "iiso_curcuit" :colors ["#f0865c" "#f2b07b" "#6bc4d2" "#1a3643"] :stroke "#0f1417" :background "#f0f0e8"}
-        {:name "iiso_airlines" :colors ["#fe765a" "#ffb468" "#4b588f" "#faf1e0"] :stroke "#1c1616" :background "#fae5c8"}
-        {:name "iiso_daily" :colors ["#e76c4a" "#f0d967" "#7f8cb6" "#1daeb1" "#ef9640"] :stroke "#000100" :background "#e2ded2"}
-
-
         {:name "jud_playground" :colors ["#f04924" "#fcce09" "#408ac9"] :stroke "#2e2925" :background "#ffffff"}
         {:name "jud_horizon" :colors ["#f8c3df" "#f2e420" "#28b3d0" "#648731" "#ef6a7d"] :stroke "#030305" :background "#f2f0e1"}
         {:name "jud_mural" :colors ["#ca3122" "#e5af16" "#4a93a2" "#0e7e39" "#e2b9bd"] :stroke "#1c1616" :background "#e3ded8"}
         {:name "jud_cabinet" :colors ["#f0afb7" "#f6bc12" "#1477bb" "#41bb9b"] :stroke "#020508" :background "#e3ded8"}
-
-
-        {:name "rag-mysore" :colors ["#ec6c26" "#613a53" "#e8ac52" "#639aa0"] :background "#d5cda1"}
-        {:name "rag-gol" :colors ["#d3693e" "#803528" "#f1b156" "#90a798"] :background "#f0e0a4"}
-        {:name "rag-belur" :colors ["#f46e26" "#68485f" "#3d273a" "#535d55"] :background "#dcd4a6"}
-        {:name "rag-bangalore" :colors ["#ea720e" "#ca5130" "#e9c25a" "#52534f"] :background "#f9ecd3"}
-        {:name "rag-taj" :colors ["#ce565e" "#8e1752" "#f8a100" "#3ac1a6"] :background "#efdea2"}
-        {:name "rag-virupagksha" :colors ["#f5736a" "#925951" "#feba4c" "#9d9b9d"] :background "#eedfa2"}
-
-
-        {:name "rohlfs_1R" :colors ["#004996" "#567bae" "#ff4c48" "#ffbcb3"] :stroke "#004996" :background "#fff8e7"}
-        {:name "rohlfs_1Y" :colors ["#004996" "#567bae" "#ffc000" "#ffdca4"] :stroke "#004996" :background "#fff8e7"}
-        {:name "rohlfs_1G" :colors ["#004996" "#567bae" "#60bf3c" "#d2deb1"] :stroke "#004996" :background "#fff8e7"}
-        {:name "rohlfs_2" :colors ["#4d3d9a" "#f76975" "#ffffff" "#eff0dd"] :stroke "#211029" :background "#58bdbc"}
-        {:name "rohlfs_3" :colors ["#abdfdf" "#fde500" "#58bdbc" "#eff0dd"] :stroke "#211029" :background "#f76975"}
-        {:name "rohlfs_4" :colors ["#fde500" "#2f2043" "#f76975" "#eff0dd"] :stroke "#211029" :background "#fbbeca"}
-
-
-        {:name "retro" :colors ["#69766f" "#9ed6cb" "#f7e5cc" "#9d8f7f" "#936454" "#bf5c32" "#efad57"] :background "#fff8e7"}
-        {:name "retro-washedout" :colors ["#878a87" "#cbdbc8" "#e8e0d4" "#b29e91" "#9f736c" "#b76254" "#dfa372"] :background "#fff8e7"}
-        {:name "roygbiv-warm" :colors ["#705f84" "#687d99" "#6c843e" "#fc9a1a" "#dc383a" "#aa3a33" "#9c4257"] :background "#fff8e7"}
-        {:name "roygbiv-toned" :colors ["#817c77" "#396c68" "#89e3b7" "#f59647" "#d63644" "#893f49" "#4d3240"] :background "#fff8e7"}
-        {:name "present-correct" :colors ["#fd3741" "#fe4f11" "#ff6800" "#ffa61a" "#ffc219" "#ffd114" "#fcd82e" "#f4d730" "#ced562" "#8ac38f" "#79b7a0" "#72b5b1" "#5b9bae" "#6ba1b7" "#49619d" "#604791" "#721e7f" "#9b2b77" "#ab2562" "#ca2847"] :background "#fff8e7"}]))
+        {:name "iiso_zeitung" :colors ["#ee8067" "#f3df76" "#00a9c0" "#f7ab76"] :stroke "#111a17" :background "#f5efcb"}
+        {:name "iiso_curcuit" :colors ["#f0865c" "#f2b07b" "#6bc4d2" "#1a3643"] :stroke "#0f1417" :background "#f0f0e8"}
+        {:name "iiso_airlines" :colors ["#fe765a" "#ffb468" "#4b588f" "#faf1e0"] :stroke "#1c1616" :background "#fae5c8"}
+        {:name "iiso_daily" :colors ["#e76c4a" "#f0d967" "#7f8cb6" "#1daeb1" "#ef9640"] :stroke "#000100" :background "#e2ded2"}
+        {:name "kov_01" :colors ["#d24c23" "#7ba6bc" "#f0c667" "#ede2b3" "#672b35" "#142a36"] :stroke "#132a37" :background "#108266"}
+        {:name "kov_02" :colors ["#e8dccc" "#e94641" "#eeaeae"] :stroke "#e8dccc" :background "#6c96be"}
+        {:name "kov_03" :colors ["#e3937b" "#d93f1d" "#090d15" "#e6cca7"] :stroke "#090d15" :background "#558947"}
+        {:name "kov_04" :colors ["#d03718" "#292b36" "#33762f" "#ead7c9" "#ce7028" "#689d8d"] :stroke "#292b36" :background "#deb330"}
+        {:name "kov_05" :colors ["#de3f1a" "#de9232" "#007158" "#e6cdaf" "#869679"] :stroke "#010006" :background "#7aa5a6"}
+        {:name "kov_06" :colors ["#a87c2a" "#bdc9b1" "#f14616" "#ecbfaf" "#017724" "#0e2733" "#2b9ae9"] :stroke "#292319" :background "#dfd4c1"}
+        {:name "kov_06b" :colors ["#d57846" "#dfe0cc" "#de442f" "#e7d3c5" "#5ec227" "#302f35" "#63bdb3"] :stroke "#292319" :background "#dfd4c1"}
+        {:name "kov_07" :colors ["#c91619" "#fdecd2" "#f4a000" "#4c2653"] :stroke "#111" :background "#89c2cd"}
+        {:name "tsu_arcade" :colors ["#4aad8b" "#e15147" "#f3b551" "#cec8b8" "#d1af84" "#544e47"] :stroke "#251c12" :background "#cfc7b9"}
+        {:name "tsu_harutan" :colors ["#75974a" "#c83e3c" "#f39140" "#e4ded2" "#f8c5a4" "#434f55"] :stroke "#251c12" :background "#cfc7b9"}
+        {:name "tsu_akasaka" :colors ["#687f72" "#cc7d6c" "#dec36f" "#dec7af" "#ad8470" "#424637"] :stroke "#251c12" :background "#cfc7b9"}
+        {:name "dt01" :colors ["#172a89" "#f7f7f3"] :stroke "#172a89" :background "#f3abb0"}
+        {:name "dt02" :colors ["#302956" "#f3c507"] :stroke "#302956" :background "#eee3d3"}
+        {:name "dt03" :colors ["#000000" "#a7a7a7"] :stroke "#000000" :background "#0a5e78"}
+        {:name "dt04" :colors ["#50978e" "#f7f0df"] :stroke "#000000" :background "#f7f0df"}
+        {:name "dt05" :colors ["#ee5d65" "#f0e5cb"] :stroke "#080708" :background "#f0e5cb"}
+        {:name "dt06" :colors ["#271f47" "#e7ceb5"] :stroke "#271f47" :background "#cc2b1c"}
+        {:name "dt07" :colors ["#6a98a5" "#d24c18"] :stroke "#efebda" :background "#efebda"}
+        {:name "dt08" :colors ["#5d9d88" "#ebb43b"] :stroke "#efebda" :background "#efebda"}
+        {:name "dt09" :colors ["#052e57" "#de8d80"] :stroke "#efebda" :background "#efebda"}
+        {:name "hilda01" :colors ["#ec5526" "#f4ac12" "#9ebbc1" "#f7f4e2"] :stroke "#1e1b1e" :background "#e7e8d4"}
+        {:name "hilda02" :colors ["#eb5627" "#eebb20" "#4e9eb8" "#f7f5d0"] :stroke "#201d13" :background "#77c1c0"}
+        {:name "hilda03" :colors ["#e95145" "#f8b917" "#b8bdc1" "#ffb2a2"] :stroke "#010101" :background "#6b7752"}
+        {:name "hilda04" :colors ["#e95145" "#f6bf7a" "#589da1" "#f5d9bc"] :stroke "#000001" :background "#f5ede1"}
+        {:name "hilda05" :colors ["#ff6555" "#ffb58f" "#d8eecf" "#8c4b47" "#bf7f93"] :stroke "#2b0404" :background "#ffda82"}
+        {:name "hilda06" :colors ["#f75952" "#ffce84" "#74b7b2" "#f6f6f6" "#b17d71"] :stroke "#0e0603" :background "#f6ecd4"}]
+       ))
 
 (defn find-palette [name]
   (->> palettes
-       (filter #(= name (:name %)))
-       (first)))
+    (filter #(= name (:name %)))
+    (first)))
